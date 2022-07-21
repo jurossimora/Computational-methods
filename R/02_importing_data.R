dbbbd0b4940f98aaa4a8e56ff5f5060bfade6446
jurossimora <jurossimora@gmail.com>
2022-07-21 12:58:46 -0300

    class about time series

--- R/02_importing_data.R
+++ R/02_importing_data.R
@@ -22,7 +22,7 @@ length(data_list)
 # Inspecting data
 head(data_list$envir)
 dim(data_list$envir)
-summary(data_list$envir)
+summary(envir$envir)
 
 # Output 1: summary table ------------------------------------------------------
 
